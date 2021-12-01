b672b98fadfa06a1a55cd4b275c40d281f4c2759
Johan Ramirez <johan911019@gmail.com>
2021-11-30 21:24:22 -0500

    Se añaden estilos y se separan componentes

--- Front/pet-project-sofkau/src/components/Users.jsx
+++ Front/pet-project-sofkau/src/components/Users.jsx
@@ -1,13 +1,40 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
+import Loading from './Loading';
+import { HOST_API } from "../config/hostApi";
 
-const Users = ({ usuarios }) => {
+const Users = () => {
+
+    const [loading, setLoading] = useState(true)
+    const [usuarios, setUsuarios] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(HOST_API + "/usuario/listar/")
+            .then((response) => {
+                setUsuarios(response.data);
+                console.log(response.data)
+                setLoading(false)
+            })
+    }, []);
+
     return (
         <Fragment>
-            {usuarios.map(usuario =>
-                <div className="mb-3 border p-2 text-center" key={usuario.id}>
-                    {usuario.nombre}
-                </div>
-            )}
+            {
+                loading
+                    ?
+                    <Loading />
+                    :
+                    <div>
+                        {usuarios.map(usuario =>
+                            <div className="d-flex justify-content-center mb-2 mt-2">
+                                <div className="mb-3 border rounded p-2 text-center" key={usuario.id}>
+                                    {usuario.nombre}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+            }
         </Fragment>
     )
 }
