81799448a228cba83bfe87c316173c955c60c9fa
Johan Ramirez <johan911019@gmail.com>
2021-12-01 12:06:41 -0500

    Se añaden validaciones en el login

--- Front/pet-project-sofkau/src/App.js
+++ Front/pet-project-sofkau/src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import Users from "./components/Users";
 import NavbarComp from "./components/NavbarComp";
+import Login from "./components/Login";
 
 function App() {
 
@@ -9,6 +10,9 @@ function App() {
       <NavbarComp />
       <h3 className="text-center mt-3">Lista de Usuarios</h3>
       <Users />
+      <div>
+        <Login />
+      </div>
     </Fragment>
   );
 }
--- Front/pet-project-sofkau/src/components/Login.jsx
+++ Front/pet-project-sofkau/src/components/Login.jsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GoogleLogin from 'react-google-login';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    // Estados Error
+    const [errorEmail, setErrorEmail] = useState(null)
+    const [errorPassword, setErrorPassword] = useState(null)
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        let mensajeError = "";
+
+        // Validaciones input iniciar sesión
+        if (!email.trim()) {
+            setErrorEmail("Ingresa un email valido")
+            return
+        }
+
+        if (password.trim()) {
+            setErrorEmail("Ingresa una contraseña valida")
+            return
+        }
+
+        // Validaciones Firebase
+        if (password.length < 6) {
+            console.error('Ingresa una constraseña mayor a 6 caracteres')
+            setErrorPassword('Ingresa una contraseña mayor a 6 caracteres')
+            return
+        }
+
+        setEmail('')
+        setPassword('')
+        setErrorEmail(null)
+        setErrorPassword(null)
+    }
 
     const responseGoogle = (response) => {
         console.log(response.profileObj);
     }
 
     return (
-        <div className="shadow-sm p-3 rounded text-center col-md-4 offset-md-4 mt-5 ">
+        <div className="shadow p-3 rounded text-center col-md-4 offset-md-4 mt-5 ">
             <nav className="navbar navbar-expand-lg navbar-light fixed-top">
                 <div className="container">
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
@@ -18,32 +53,63 @@ const Login = () => {
 
             <div className="auth-wrapper">
                 <div className="auth-inner">
-                    <form>
-                        <h3>Iniciar Session</h3>
+                    <form onSubmit={handleSubmit}>
+
+                        <h3 className="text-decoration-underline p-2">Iniciar Sesión</h3>
 
                         <div className="form-group p-3">
-                            <label>Correo Electronico</label>
-                            <input type="email" className="form-control" placeholder="Ingrese correo electronico" />
+                            <label>Correo Electrónico</label>
+                            <input
+                                type="email"
+                                className={`form-control mb-2 ` + (errorEmail ? 'is-invalid' : '')}
+                                placeholder="Ingresa tu email"
+                                onChange={e => setEmail(e.target.value)}
+                                value={email}
+                            />
+                            {
+                                errorEmail ?
+                                    (
+                                        <div className="invalid-feedback mb-2">
+                                            {errorEmail}
+                                        </div>
+                                    )
+                                    : null
+                            }
                         </div>
 
                         <div className="form-group  p-3">
                             <label>Contraseña</label>
-                            <input type="password" className="form-control" placeholder="Ingrese contraseña" />
+                            <input
+                                type="password"
+                                className={`form-control mb-2 ` + (errorPassword ? 'is-invalid' : '')}
+                                placeholder="Ingresa tu contraseña"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                            {
+                                errorPassword ?
+                                    (
+                                        <div className="invalid-feedback mb-2">
+                                            {errorPassword}
+                                        </div>
+                                    )
+                                    : null
+                            }
                         </div>
 
                         <div className="form-group  p-3">
                             <div className="custom-control custom-checkbox">
                                 <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                                <label className="custom-control-label" htmlFor="customCheck1">Recuerdame</label>
+                                <label className="custom-control-label m-2" htmlFor="customCheck1">Recuerdame</label>
                             </div>
                         </div>
 
-                        <button type="submit" className="btn btn-primary btn-block">Iniciar</button>
+                        <button type="submit" className="btn btn-dark btn-block">¡Vamos!</button>
                         <br></br>
-                        <div className="form-group  p-3">
+                        <div className="form-group p-3">
                             <GoogleLogin
                                 clientId="438215108595-f7lfe4tomh8bbpm3n30kj6q0bonhp705.apps.googleusercontent.com"
-                                buttonText="Login"
+                                buttonText="Iniciar con Google"
                                 onSuccess={responseGoogle}
                                 onFailure={responseGoogle}
                                 cookiePolicy={'single_host_origin'}
